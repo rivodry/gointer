c35d5fc30445db2a79a2031f001bc90851f8306f
rivodry <212361889+rivodry@users.noreply.github.com>
2025-06-17 13:14:45 +0530

    Some thing

--- src/frontend/main.go
+++ src/frontend/main.go
@@ -7,13 +7,21 @@ import (
 	"os"
 )
 
-func main() {
-	args := os.Args[1:]
-	var asts []*ast.File
+func ParseImport(args []string) [][]*ast.ImportSpec {
+	var asts [][]*ast.ImportSpec
+
 	for _, file := range args {
 		dat, _ := os.ReadFile("./" + file)
 		a, _ := parser.ParseFile(&token.FileSet{}, file, dat, 0)
-		asts = append(asts, a)
+
+		asts = append(asts, a.Imports)
+
 	}
+	return asts
+}
+func main() {
+	args := os.Args[1:]
+	asts := ParseImport(args)
+
 	ast.Print(&token.FileSet{}, asts)
 }
